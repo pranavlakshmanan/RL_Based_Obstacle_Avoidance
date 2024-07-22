913377df40608573c80f419fbd37109f6ba43d11
Pranav_L <113857566+pranavlakshmanan@users.noreply.github.com>
2024-07-22 19:57:00 +0530

    Update hardwarev1.py

--- hardwarev1.py
+++ hardwarev1.py
@@ -1,3 +1,8 @@
+# This code is meant to run on the onboard Raspberry Pi of a TurtleBot 3
+# which is connected to a laptop using an SSH connection. The code
+# subscribes to the /odom and /scan topics of the robot and publishes
+# to the /cmd_vel topic to control the robot's movement.
+
 import time
 import numpy as np
 import torch
@@ -9,47 +14,59 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 import math
 
+# Define the Actor neural network
 class Actor(nn.Module):
     def __init__(self, state_dim, action_dim):
         super(Actor, self).__init__()
         
+        # Define the layers of the neural network
         self.layer_1 = nn.Linear(state_dim, 800)
         self.layer_2 = nn.Linear(800, 600)
         self.layer_3 = nn.Linear(600, action_dim)
         self.tanh = nn.Tanh()
     
     def forward(self, s):
+        # Forward pass through the network
         s = F.relu(self.layer_1(s))
         s = F.relu(self.layer_2(s))
         a = self.tanh(self.layer_3(s))
         return a
 
+# Define the TD3 (Twin Delayed Deep Deterministic Policy Gradient) agent
 class TD3(object):
     def __init__(self, state_dim, action_dim):
         self.actor = Actor(state_dim, action_dim)
     
     def get_action(self, state):
+        # Get action from the actor network
         state = torch.FloatTensor(state.reshape(1, -1))
         return self.actor(state).cpu().data.numpy().flatten()
     
     def load(self, filename, directory):
+        # Load pre-trained model weights
          self.actor.load_state_dict(
              torch.load("%s/%s_actor.pth" % (directory, filename), map_location=torch.device('cpu'))
          )
 
+# Main TurtleBot controller class
 class TurtleBotController:
     def __init__(self):
         rospy.init_node('turtlebot_controller', anonymous=True)
         
+        # Set up state and action dimensions
         self.state_dim = 24  # 20 laser readings + 4 odometry values
         self.action_dim = 2
+        
+        # Initialize the TD3 network and load pre-trained weights
         self.network = TD3(self.state_dim, self.action_dim)
         self.network.load("TD3_velodyne", "./pytorch_models")
         
+        # Set up ROS publishers and subscribers
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
         self.laser_sub = rospy.Subscriber('/scan', LaserScan, self.laser_callback)
         
+        # Initialize data holders
         self.odom_data = None
         self.laser_data = None
         
@@ -64,12 +81,15 @@ class TurtleBotController:
         self.min_obstacle_distance = 0.5  # Minimum allowed distance to obstacles
     
     def odom_callback(self, data):
+        # Callback function for odometry data
         self.odom_data = data
     
     def laser_callback(self, data):
+        # Callback function for laser scan data
         self.laser_data = data
     
     def get_state(self):
+        # Prepare the state for the neural network
         if self.odom_data is None or self.laser_data is None:
             return None
         
@@ -102,6 +122,8 @@ class TurtleBotController:
             state_info = self.get_state()
             if state_info is not None:
                 nn_state, distance_to_target, angle_to_target = state_info
+                
+                # Get action from the neural network
                 action = self.network.get_action(nn_state)
                 
                 # Convert action to Twist message
@@ -128,6 +150,7 @@ class TurtleBotController:
                     twist_msg.linear.x = 0
                     twist_msg.angular.z = 0.5  # Turn in place to avoid obstacle
                 
+                # Publish the command velocity
                 self.cmd_vel_pub.publish(twist_msg)
             
             self.rate.sleep()
